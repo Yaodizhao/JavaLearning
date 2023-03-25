cd7590983820930551d504766c02eb23198b382d
如月车站 <yaodizhao2021@geothinking.cn>
2023-03-25 20:29:53 +0800

    add search

--- src/数据结构/Search.java
+++ src/数据结构/Search.java
@@ -1,5 +1,7 @@
 package 数据结构;
 
+import java.util.Arrays;
+
 /**
  * @author 如月车站
  */
@@ -8,7 +10,7 @@ public class Search {
         int[] array = new int[]{1, 2, 4, 8, 9, 31, 52, 61, 71, 102};
 //        int[] a = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Search search = new Search();
-        int result = search.binarySearch(array, 71);
+        int result = search.fibonacciSearch(array, 52);
         System.out.println(result);
     }
 
@@ -18,7 +20,7 @@ public class Search {
                 return i;
             }
         }
-        return target;
+        return -1;
     }
 
     public int binarySearch(int[] nums, int target) {
@@ -34,7 +36,66 @@ public class Search {
                 high = mid - 1;
             }
         }
-        return target;
+        return -1;
     }
 
+    /**
+     * 插值查找
+     *
+     * @param nums   数组
+     * @param target 查找值
+     * @return 查找结果
+     */
+    public int interpolationSearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) * (target - nums[low]) / (nums[high] - nums[low]);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int fibonacciSearch(int[] nums, int target) {
+
+        int[] fibonacci = fibonacci();
+        int low = 0;
+        int high = nums.length - 1;
+        int k = 0;
+        int mid = 0;
+        while (fibonacci[k] - 1 < high) {
+            k++;
+        }
+        int[] temp = Arrays.copyOf(nums, fibonacci[k]);
+        Arrays.fill(temp, high, temp.length, nums[high]);
+        while (low < high) {
+            mid = low + fibonacci[k - 1] - 1;
+            if (temp[mid] == target) {
+                return mid;
+            } else if (temp[mid] > target) {
+                low = mid + 1;
+                k -= 2;
+            } else {
+                high = mid - 1;
+                k--;
+            }
+        }
+        return -1;
+    }
+
+    public int[] fibonacci() {
+        int[] nums = new int[20];
+        nums[0] = 1;
+        nums[1] = 1;
+        for (int i = 2; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+        return nums;
+    }
 }
